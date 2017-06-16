3333843a2818bcedf56b3ab50b484897a747b989
WarrenWFu <fuyiminshenzhen@outlook.com>
2017-06-16 17:58:36 +0800

    Signed-off-by: WarrenWFu <fuyiminshenzhen@outlook.com>

--- go/src/mygithub/WarrenWFu/http/saysomething/main.go
+++ go/src/mygithub/WarrenWFu/http/saysomething/main.go
@@ -13,6 +13,8 @@ type WebPage struct {
 	Body  []byte
 }
 
+var templates = template.Must(template.ParseFiles("index.html"))
+
 func readHtml(filename string) (*WebPage, error) {
 	bytes, err := ioutil.ReadFile(filename + ".html")
 	if err != nil {
@@ -23,40 +25,23 @@ func readHtml(filename string) (*WebPage, error) {
 }
 
 func viewMain(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Println("method", r.Method)
-	//	fmt.Printf("URL[%v]\n", r.URL)
-	//	fmt.Printf("请求头[%v]\n", r.Header)
-	//	bsBody := make([]byte, 1024)
-	//	r.Body.Read(bsBody)
-	//	fmt.Printf("请求体[%s]\n", bsBody)
-
 	if r.URL.Path == "/" {
 		http.Redirect(w, r, "/index", http.StatusFound)
 	} else if r.URL.Path == "/index" {
-		if webPage, err := readHtml("main"); err != nil {
-			fmt.Printf("处理[%s]请求失败，原因[%s]\n", "main", err)
-			w.Write([]byte("page not found"))
-		} else {
-			w.Write(webPage.Body)
-		}
+		webPage := &WebPage{Body: []byte("说点什么吧")}
+		templates.ExecuteTemplate(w, "index.html", webPage)
 	} else if r.URL.Path == "/said" {
-		if t, err := template.ParseFiles("./main.html"); err != nil {
-			fmt.Printf("处理[%s]请求失败，原因[%s]\n", "main", err)
-			w.Write([]byte("page not found"))
-		} else {
-			body := r.FormValue("text1")
-			//bodyType := reflect.ValueOf(body)
-			//fmt.Printf("%s, %s\n", body, bodyType.Type())
-			webPage := &WebPage{Body: []byte(body)}
-			t.Execute(w, webPage)
-		}
+		webPage := &WebPage{Body: []byte(r.FormValue("text1"))}
+		templates.ExecuteTemplate(w, "index.html", webPage)
 	} else {
-		w.Write([]byte("page not found"))
+		http.NotFound(w, r)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", viewMain)
 
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("启动监听失败，原因[%s]\n", err)
+	}
 }
