f0d3c2019645a084f176fded5c92c2ec4d19d7ea
WarrenWFu <fuyiminshenzhen@outlook.com>
2019-04-28 12:17:19 +0800

    commit

--- javascript/node.js/test.js
+++ javascript/node.js/test.js
@@ -347,5 +347,8 @@ sleep(3000);
 console.log('yes');
  */
 
-console.log();
-console.log();
+//实现一个简单的tcp服务器
+require('http').createServer(function (req, res) {
+  //res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello <i>World</i></h1>');
+}).listen(3000);
