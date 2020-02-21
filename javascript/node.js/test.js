6383862eb09c549e939d8059131686504f0166b2
WarrenWFu <fuyiminshenzhen@outlook.com>
2020-02-21 20:12:47 +0800

    commit

--- javascript/node.js/test.js
+++ javascript/node.js/test.js
@@ -431,7 +431,7 @@ function getIPAdress() {
 }
 console.log(getIPAdress());
  */
-
+/* 
 //对象和数组都可以使用forin，要区分还是要通过instanceof或typeof
 var foo = {
   v1: 'a',
@@ -450,6 +450,52 @@ for (v in bar) {
 }
 
 console.log(foo instanceof Array);
-console.log(typeof foo);
+console.log(typeof foo); //返回object
 console.log(bar instanceof Array);
-console.log(typeof bar);
+console.log(typeof bar); //返回object
+ */
+
+
+var MyModules = (function Manager() {
+  var modules = {};
+  function define(name, deps, impl) {
+    for (var i = 0; i < deps.length; i++) {
+      deps[i] = modules[deps[i]];
+    }
+    //modules[name] = impl.apply(impl, deps);
+    modules[name] = impl(deps);
+  }
+  function get(name) {
+    return modules[name];
+  }
+  return {
+    define: define,
+    get: get
+  };
+})();
+
+MyModules.define('bar', [], function () {
+  function hello(who) {
+    return 'Let me introduce: ' + who;
+  }
+  return {
+    hello: hello
+  };
+});
+
+MyModules.define('foo', ['bar'], function (bar) {
+  var hungry = 'hippo';
+  function awesome() {
+    console.log(bar.hello(hungry).toUpperCase());
+  }
+  return {
+    awesome: awesome
+  };
+});
+
+var bar = MyModules.get('bar');
+var foo = MyModules.get('foo');
+console.log(
+  bar.hello('hippo')
+); // Let me introduce: hippo
+foo.awesome(); // LET ME INTRODUCE: HIPPO
